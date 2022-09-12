eea30ff9340f6834dbb3d58fd3dd3b676e2fc002
shubham kathare <shubhamkathare624@gmail.com>
2022-09-12 15:51:03 +0530

    First Commit

---
+++ src/main/java/com/text/book/AngularprojApplication.java
@@ -0,0 +1,13 @@
+package com.text.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/text/book/AngularprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.text.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
